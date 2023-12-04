d90cd3820aecd799f2864b396bbe0875ccf03cd3
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-12-03 17:14:33 -0800

    update for new GPS

--- gps.py
+++ gps.py
@@ -1,41 +1,23 @@
-# This is the code for GPS connection, inspired Arijit Das' article
+# This is the code for GPS connection, inspired Abdullah Jirjees
+# https://github.com/AbdullahJirjees/VK-16_GPS/tree/main
 
 # pynmea library used for NMEA 0183 protocol, a data specification for
 # communication between marine electronics including GPS receivers. 
 # pip install pynmea2
 
-# This module encapsulates the access for the serial port.
 import serial
-
-# This module provide various time-related functions
-import time 
-import string
 import pynmea2
 
-# Infinite loop to continuously read GPS data
-while True: 
-    # Specify the serial port on Raspberry Pi
-    port = '/dev/ttyAMA0'
+def read_gps_data(serial_port='COM3', baudrate=9600):
+    # Set serial port, baudrate, and timeout
 
-    # Initialize a serial connection with the specified port, baudrate, and timeout
-    # 9600 baudrate is the default for many GPS modules
-    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
+    ser = serial.Serial(serial_port, baudrate=baudrate, timeout=1)
+    while True:
+        # read data and parse data message
+        data = ser.readline()
+        if data.startswith(b'$GPGGA'):
+            msg = pynmea2.parse(data.decode('utf-8'))
+            print(f"Latitude: {msg.latitude}, Longitude: {msg.longitude}")
 
-    # Create an instance of NMEAStreamReader for parsing NMEA sentences
-    dataOut = pynmea2.NMEAStreamReader()
-
-    # Read a line of NMEA sentence from the serial port
-    newData = ser.readline()
-
-    # Check if the NMEA sentence starts with '$GPRMC'
-    if newData[0:6] == '$GPRMC':
-        # Parse the NMEA sentence, create a message object with GPS data
-        newMsg = pynmea2.parse(newData)
-
-        # Extract latitude and longitude information from the parsed message
-        lat = newMsg.latitude
-        long = newMsg.longtitude
-
-        # Print the GPS coordinates 
-        gpsCoord = 'Latitude=' + str(lat) + 'Longtitude=' + str(long)
-        print(gpsCoord)
+if __name__ == "__main__":
+    read_gps_data()
