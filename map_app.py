17404a6406fb40f896c222883995b5f9c2056d05
Ezra Hsieh <ezrahsieh@yahoo.com>
2023-12-06 16:09:30 -0800

    added speed limit api

--- map_app.py
+++ map_app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 import os
 from datetime import datetime
+import requests
+
 
 app = Flask(__name__)
+
+# Enter your own bing maps api key
+bing_maps_api_key = 'Au6Am6m_YLRyDRC4vQ94eTWBf4hqsGbG-e6GIeVcuoEOj9RVyGboeEdcy1lzCphk'
 
 @app.route('/')
 def map_page():
@@ -12,8 +17,10 @@ def map_page():
     coordinates = []
     date = []
     time = []
+    limit = []
 
     # Specify the file path on the flash drive
+    # flash_drive_path = 'D:\\velocity_violations.txt'
     flash_drive_path = 'D:\\velocity_violations.txt'
     
     # Check if the file exists before trying to open it
@@ -61,12 +68,48 @@ def map_page():
                 miph = round(miph, 2)
                 velocity.append(miph)
 
+                # Use the Bing Maps API to get the speed limit
+                speed_limit = get_speed_limit(coordinate)
+                if speed_limit:
+                    # Compare the recorded speed with the speed limit
+                    if miph > speed_limit:
+                        print(f"Speed limit exceeded: {miph} MPH in a {speed_limit} MPH zone")
+                    limit.append(speed_limit)
+                else:
+                    limit.append(0)
+
+                        
+
         # Pass data to the template for rendering
-        print(violation_number)
-        print(velocity)
-        return render_template('map.html', coordinates=coordinates, violation_number=violation_number, date=date, time=time, velocity=velocity)
+        return render_template('map.html', coordinates=coordinates, violation_number=violation_number, date=date, time=time, velocity=velocity, limit=limit)
     else:
         return "File not found on the specified flash drive path."
+    
+def get_speed_limit(coordinate):
+    # Use the Bing Maps Roads API to get the speed limit for the given coordinate
+    base_url = 'https://dev.virtualearth.net/REST/v1/Routes/SnapToRoad'
+    params = {
+        'key': bing_maps_api_key,
+        'points': coordinate,
+        'includeSpeedLimit': 'true',
+        'speedUnit': 'MPH',
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    print(data)
+
+    if 'resourceSets' in data and data['resourceSets'] and 'resources' in data['resourceSets'][0]:
+        # Extract speed limit from the response (assuming the first resource is the most relevant)
+        speed_limit = data['resourceSets'][0]['resources'][0]['snappedPoints'][0]['speedLimit']
+        print("Speed limit: ", speed_limit)
+        return speed_limit
+    else:
+        # If speed limit information is not available, return None
+        return None
+    
+
 
 if __name__ == '__main__':
     # Run the Flask application in debug mode
